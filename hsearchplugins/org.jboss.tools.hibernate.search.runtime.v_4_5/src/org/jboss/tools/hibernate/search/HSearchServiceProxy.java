d0163d2e17bd6562be1812ea64b16a45d3d6df47
Koen Aers <koen.aers@gmail.com>
2015-06-17 11:44:06 +0200

    Comment out the extension point, replace ServiceProxy by ServiceImpl

--- hsearchplugins/org.jboss.tools.hibernate.search.runtime.v_4_5/src/org/jboss/tools/hibernate/search/HSearchServiceProxy.java
+++ hsearchplugins/org.jboss.tools.hibernate.search.runtime.v_4_5/src/org/jboss/tools/hibernate/search/HSearchServiceProxy.java
@@ -3,17 +3,17 @@ package org.jboss.tools.hibernate.search;
 import org.hibernate.internal.SessionFactoryImpl;
 import org.hibernate.search.FullTextSession;
 import org.hibernate.search.Search;
-import org.jboss.tools.hibernate.proxy.ServiceProxy;
-import org.jboss.tools.hibernate.proxy.SessionFactoryProxy;
+import org.jboss.tools.hibernate.runtime.common.IFacade;
 import org.jboss.tools.hibernate.runtime.spi.ISessionFactory;
+import org.jboss.tools.hibernate.runtime.v_4_3.internal.ServiceImpl;
 import org.jboss.tools.hibernate.search.runtime.spi.IHSearchService;
 
-public class HSearchServiceProxy extends ServiceProxy implements IHSearchService {
+public class HSearchServiceProxy extends ServiceImpl implements IHSearchService {
 	
 	@Override
 	public void newIndexRebuild(ISessionFactory sessionFactory) {
 		try {
-			SessionFactoryImpl factory = (SessionFactoryImpl) ((SessionFactoryProxy) sessionFactory).getTarget();
+			SessionFactoryImpl factory = (SessionFactoryImpl) ((IFacade) sessionFactory).getTarget();
 			FullTextSession fullTextSession = Search.getFullTextSession(factory.getCurrentSession());
 			fullTextSession.createIndexer().startAndWait();
 		} catch (InterruptedException e) {
